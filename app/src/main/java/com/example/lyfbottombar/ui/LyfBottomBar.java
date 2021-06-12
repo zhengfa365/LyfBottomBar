33d66359b476c464d92bdc28d17609fba5bd13ac
lyf <515003570@qq.com>
2021-06-13 02:36:43 +0800

    add project basic file

---
+++ app/src/main/java/com/example/lyfbottombar/ui/LyfBottomBar.java
@@ -0,0 +1,14 @@
+package com.example.lyfbottombar.ui;
+
+/**
+ * *******************
+ * 项目名称 LyfBottomBar
+ *
+ * @Author lyf
+ * email:869824341@qq.com
+ * 创建时间 2021/6/13 2:23
+ * 用途
+ * *******************
+ */
+class LyfBottomBar {
+}
